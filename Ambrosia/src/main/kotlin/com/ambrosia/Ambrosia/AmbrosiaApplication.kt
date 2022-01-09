1e95772fcd54bfd73165709fbc0b1359a79f598d
Reda Benchraa <redabenchraa@gmail.com>
2022-01-09 22:30:29 +0100

    feat: #1 init project using Kotlin, Spring boot, Gradle and basic dependencies

---
+++ Ambrosia/src/main/kotlin/com/ambrosia/Ambrosia/AmbrosiaApplication.kt
@@ -0,0 +1,11 @@
+package com.ambrosia.Ambrosia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AmbrosiaApplication
+
+fun main(args: Array<String>) {
+	runApplication<AmbrosiaApplication>(*args)
+}
---
+++ Ambrosia/src/test/kotlin/com/ambrosia/Ambrosia/AmbrosiaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ambrosia.Ambrosia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AmbrosiaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
