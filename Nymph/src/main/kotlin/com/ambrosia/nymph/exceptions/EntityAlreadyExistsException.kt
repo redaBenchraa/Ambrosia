b76ad749c8dee3e78ec47fcc6acb502f73c035c0
Reda Benchraa <redabenchraa@gmail.com>
2022-01-10 17:49:35 +0100

    Add config for keycloak

---
+++ Nymph/src/main/kotlin/com/ambrosia/nymph/exceptions/EntityAlreadyExistsException.kt
@@ -0,0 +1,5 @@
+package com.ambrosia.nymph.exceptions
+
+class EntityAlreadyExistsException(
+    entityClass: Class<*>, vararg searchParamsMap: Any
+) : EntityException(entityClass, searchParamsMap)
