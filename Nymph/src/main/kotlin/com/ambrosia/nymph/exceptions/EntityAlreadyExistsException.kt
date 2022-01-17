8ecc108f038beccaa5a130c741566f91dcaf137a
Reda Benchraa <redabenchraa@gmail.com>
2022-01-17 09:41:34 +0100

    Improve error handling

--- Nymph/src/main/kotlin/com/ambrosia/nymph/exceptions/EntityAlreadyExistsException.kt
+++ Nymph/src/main/kotlin/com/ambrosia/nymph/exceptions/EntityAlreadyExistsException.kt
@@ -2,4 +2,4 @@ package com.ambrosia.nymph.exceptions
 
 class EntityAlreadyExistsException(
 	entityClass: Class<*>, vararg searchParamsMap: Any
-) : EntityException(entityClass, searchParamsMap)
+) : EntityException(entityClass, *searchParamsMap)
--- Nymph/src/main/kotlin/com/ambrosia/nymph/exceptions/EntityException.kt
+++ Nymph/src/main/kotlin/com/ambrosia/nymph/exceptions/EntityException.kt
@@ -3,26 +3,21 @@ package com.ambrosia.nymph.exceptions
 import java.util.stream.IntStream
 
 
-abstract class EntityException(
-	val entityClass: Class<*>, vararg searchParamsMap: Any
-) :
-	RuntimeException() {
+abstract class EntityException(val entityClass: Class<*>, vararg searchParamsMap: Any) : RuntimeException() {
 	@Transient
 	val parameters: Map<String, Any>
 
 	init {
-		parameters = toMap(searchParamsMap)
+		parameters = toMap(*searchParamsMap)
 	}
 
-	companion object {
-		private fun toMap(vararg entries: Any): Map<String, Any> {
-			require(entries.size % 2 != 1) { "Invalid entries" }
-			return IntStream.range(0, entries.size / 2)
-				.map { i: Int -> i * 2 }
-				.collect(
-					{ HashMap() },
-					{ m: HashMap<String, Any>, i: Int -> m[entries[i] as String] = entries[i + 1] }
-				) { obj: HashMap<String, Any>, m: HashMap<String, Any> -> obj.putAll(m) }
-		}
+	private fun toMap(vararg entries: Any): Map<String, Any> {
+		require(entries.size % 2 != 1) { "Invalid entries" }
+		return IntStream.range(0, entries.size / 2)
+			.map { i: Int -> i * 2 }
+			.collect(
+				{ HashMap() },
+				{ m: HashMap<String, Any>, i: Int -> m[entries[i] as String] = entries[i + 1] }
+			) { obj: HashMap<String, Any>, m: HashMap<String, Any> -> obj.putAll(m) }
 	}
 }
