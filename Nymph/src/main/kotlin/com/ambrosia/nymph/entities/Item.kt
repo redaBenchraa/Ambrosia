ef319d73e56b255ee09d0306a5a2c674ccd12a88
Reda Benchraa <redabenchraa@gmail.com>
2022-01-23 23:45:34 +0100

    Set delete strategy and fix deleted

--- Nymph/src/main/kotlin/com/ambrosia/nymph/entities/Item.kt
+++ Nymph/src/main/kotlin/com/ambrosia/nymph/entities/Item.kt
@@ -3,24 +3,25 @@ package com.ambrosia.nymph.entities
 import com.ambrosia.nymph.constants.Constants
 import com.ambrosia.nymph.constants.Constants.Companion.NAME_MAX_SIZE
 import com.fasterxml.jackson.annotation.JsonManagedReference
-import org.hibernate.annotations.ColumnDefault
-import org.hibernate.annotations.OnDelete
-import org.hibernate.annotations.OnDeleteAction
+import org.hibernate.annotations.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
 import javax.persistence.*
+import javax.persistence.Entity
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 @Entity
+@SQLDelete(sql = "UPDATE item SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
 class Item(
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
     @field:NotNull(message = "error.item.id.null")
-    @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null,
     @field:NotNull(message = "error.item.name.null")
     @field:NotBlank(message = "error.item.name.blank")
@@ -43,10 +44,11 @@ class Item(
     @LastModifiedDate
     @ColumnDefault(Constants.NOW)
     var updatedAt: LocalDateTime = LocalDateTime.now(),
+    @Column(columnDefinition = "boolean default 0")
     var deleted: Boolean = false,
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "business_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    @JoinColumn(name = "business_id", nullable = true)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonManagedReference
     var business: Business? = null,
 )
